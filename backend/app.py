d85ad1be8a5b6a6286ad5f5e5178fed60949ed0e
Ubuntu <azure@elice-kdt-ai-track-vm-da-01.hqbktzrxcscu3ivrumcvgloqqd.syx.internal.cloudapp.net>
2021-04-10 08:59:55 +0000

    sqlAlchemy 초기설정 code 추가

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,22 @@
 from flask import Flask 
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 
+'''
+# sqlAlchemy 관련 코드
+# config.py 설정파일
+app.config.from_object('config')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'flasknotewithsqlalchemy'
+db = SQLAlchemy(app)
+'''
+
+# from app import views, models
+
 @app.route('/') 
-def hello(): return 'Hello, World!'
+def hello(): 
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True, threaded=False)
