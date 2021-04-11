80231b7f817929af5369accb4b0f8bd52ea4686c
다인 김 <dainlinda@gmail.com>
2021-04-11 15:25:02 +0900

    코드 곁가지 정리

--- backend/app.py
+++ backend/app.py
@@ -55,17 +55,5 @@ class BalanceGameOptions(Resource):
 
 api.add_resource(BalanceGameOptions, '/games/balance/option')
 
-# # 밸런스 게임 문제
-# class BalanceGameOptions(Resource):
-#     @swag_from("swagger_config/ramdom_data.yml")
-#     def get(self):
-#         result = db.balance_game_options_select()
-#         options = []
-#         for row in result:
-#             options.append({'id': row[0], 'option': row[1], 'image': row[2]})
-#         return jsonify(options = options)
-
-# api.add_resource(BalanceGameOptions, '/letters/random/data')
-
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True, threaded=False)
